b5556914f9bdaa7fa57a8bef992b24d5ec6eaa02
cryptotopstar <tomason.lee.940928@gmail.com>
2023-04-04 08:27:44 -0700

    changes to driver

--- driver.py
+++ driver.py
@@ -12,13 +12,18 @@ PATH2 = "./Cache/Objects/SERVER_NAMES.json"
 extProc = sp.Popen(['python3','commands.py'])
 lastMod = os.path.getmtime(PATH)
 lastMod2 = os.path.getmtime(PATH2)
+counter = 1
+
 ## Execute only if the file has been changed
 while True:
-    if lastMod != os.path.getmtime(PATH) or lastMod2 != os.path.getmtime(PATH2):
-        time.sleep(10)
+    if lastMod != os.path.getmtime(PATH) or lastMod2 != os.path.getmtime(PATH2) or counter % 10000 == 0:
+        extProc.kill()
         lastMod = os.path.getmtime(PATH)
         lastMod2 = os.path.getmtime(PATH2)
         sp.Popen.terminate(extProc)
         extProc = sp.Popen(['python3','commands.py'])
+        counter = 1
+        time.sleep(10)
     else:
         time.sleep(10)
+        counter += 1
